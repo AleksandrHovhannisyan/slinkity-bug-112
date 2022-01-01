c813bbd393ab5d11e77fe65d2ad28b6d001181dd
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-01-01 10:00:05 -0500

    Demo https://github.com/slinkity/slinkity/issues/112

---
+++ src/_includes/components/Test/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const Test = () => <div className={styles.test} />
+
+export default Test;
---
+++ src/index.md
@@ -0,0 +1,5 @@
+---
+layout: layout
+---
+
+{% react 'components/Test/index', render='static' %}
