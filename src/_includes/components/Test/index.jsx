b5069edb8795e7d9d54ad5eb2e41b992d9348a28
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-01-01 10:05:07 -0500

    Simplify

--- src/_includes/components/Test/index.jsx
+++ src/_includes/components/Test/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import styles from './styles.module.css';
+import './styles.css';
 
-const Test = () => <div className={styles.test} />
+const Test = () => <div />
 
 export default Test;
